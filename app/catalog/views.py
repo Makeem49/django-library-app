1be3cb80d8187d50bb7781b6236f4fb666166299
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-26 12:52:43 +0100

    adding logic to view function

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,13 +1,38 @@
+from pydoc import render_doc
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Author, Book, BookInstance, Genre
 
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse('Hello wolrd, this is my first django web application.')
+    # creating a home view handling incoming http request
+
+    number_of_author = Author.objects.all().count()
+    num_of_books = Book.objects.all().count()
+    num_of_bookInstance_available = BookInstance.objects.filter(
+        status__exact='a').count()
+    number_of_bookInstance = BookInstance.objects.all().count()
+    num_of_genre = Genre.objects.all().count()
+    contain_genre = Book.objects.filter(summary__icontains='genre ').all().count()
+
+
+    context = {
+        'number_of_author': number_of_author,
+        'num_of_books': num_of_books,
+        'num_of_bookInstance_available': num_of_bookInstance_available,
+        'number_of_bookInstance': number_of_bookInstance,
+        'num_of_genre' : num_of_genre,
+        'contain_genre' : contain_genre
+    }
+
+    return render(request, 'home.html', context=context)
+
 
 def about(request):
     return HttpResponse('About page')
+
 
 def faq(request):
     return HttpResponse('Frequestly asked question page')

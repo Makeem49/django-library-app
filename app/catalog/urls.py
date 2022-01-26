9c1b1bbd107f39f71b67baf50d3b79407cfd38fc
makeem49 <moshoodakeemolayinka@gmail.com>
2022-01-26 12:51:43 +0100

    change route name to catalog-home

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-name'),
+    path('', views.home, name='catalog-home'),
     path('frequent-questions', views.faq, name='blog-faq'),
     path('about', views.about, name='blog-about')
 ]

34daa0b9a2f5b882d422d30dab254ef6cca0875d
makeem49 <moshoodakeemolayinka@gmail.com>
2022-02-08 21:14:01 +0100

    adding view

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='catalog-home'),
-    path('frequent-questions', views.faq, name='blog-faq'),
-    path('about', views.about, name='blog-about')
+    path('author-details/', views.AuthorList.as_view(), name='catalog-authors'),
+    # path('books/', views.books, name='catalog-books'),
+    path('books/', views.BookListView.as_view(), name='catalog-books'),
+    path('book-detail/<int:pk>', views.get_book_detail, name='catalog-book-detail'),
+    path('author-detail/<int:pk>', views.AuthorDetail.as_view(), name='catalog-author-detail')
 ]
 
